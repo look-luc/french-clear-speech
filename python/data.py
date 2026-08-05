f4680b03ba7d0c7b3414e033791cb177ca62e1b7
look-luc <denardiluc@gmail.com>
2026-08-05 14:21:36 -0600

    making get data and data collate

--- python/data.py
+++ python/data.py
@@ -1,2 +1,48 @@
-def get_data():
-    pass
+from pathlib import Path
+
+import numpy as np
+import torch
+import torchaudio
+import torchaudio.transforms as T
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+class Data:
+    def __init__(
+        self,
+        processor,
+        feature_extractor,
+        audio_path:str=f"{BASE_DIR}/praat/data",
+        txt_path:str=f"{BASE_DIR}/praat/data",
+    ) -> None:
+        self.processor = processor
+        self.feature_extractor = feature_extractor
+        self.audio_path = audio_path
+        self.txt_path = txt_path
+
+    def get_data(self):
+        wav_files = [p for p in Path(self.audio_path).iterdir() if p.is_file() and str(p)[-4:]==".wav"]
+        txt_files = [p for p in Path(self.audio_path).iterdir() if p.is_file() and str(p)[-4:]==".txt"]
+
+        labels = []
+        audio_files = []
+        for wav, txt in zip(wav_files, txt_files):
+            waveform, sample_rate = torchaudio.load(str(wav))
+
+            if sample_rate != 16000:
+                resampler = T.Resample(orig_freq=sample_rate, new_freq=16000)
+                waveform = resampler(waveform)
+
+            transcript = np.loadtxt(txt, delimiter=" ")
+            transcript_tensor = torch.from_numpy(transcript)
+
+            waveform_1d = waveform.squeeze(0)
+
+            outputs = self.feature_extractor(waveform_1d, sampling_rate=16000, return_tensors="pt")
+            extracted_features = outputs.input_features[0]
+
+            label_ids = self.processor.tokenizer(transcript_tensor).input_ids
+
+            audio_files.append(extracted_features)
+            labels.append(label_ids)
+
+        return {"input_features": audio_files, "labels": labels}
--- python/model.py
+++ python/model.py
@@ -2,31 +2,48 @@ import sys
 from pathlib import Path
 
 import torch
-from transformers import WhisperForConditionalGeneration, WhisperProcessor
+from transformers import (
+    AutoFeatureExtractor,
+    WhisperForConditionalGeneration,
+    WhisperProcessor,
+)
 
 root_dir = Path(__file__).resolve().parents[3]
 if str(root_dir) not in sys.path:
     sys.path.append(str(root_dir))
 
-from .data import get_data
+from .data import Data
 
+
+def data_collate(batch, processor, feature_extractor):
+    feature_list = [item["input_feature"] for item in batch]
+    label_list = [item["labels"] for item in batch]
+
+    padding_inputs = feature_extractor.pad(feature_list, return_tensors='pt')
+    padded_labels = feature_extractor.pad(label_list, return_tensors='pt', padding_value=-100)
+
+    return {
+        "input_features": padding_inputs.input_features,
+        "labels": padded_labels.input_ids
+    }
 
 class French_Speech_text:
     def __init__(
         self,
         model_id:str="bofenghuang/whisper-medium-french",
         level_tweak:float=0.0,
-        device:str="cuda:0" if torch.cuda.is_available() else "mps:1" if  torch.backends.mps.is_available() else "cpu:3",
+        device:str="cuda:0" if torch.cuda.is_available() else "mps:1" if  torch.backends.mps.is_available() else "cpu:2",
     ) -> None:
         self.device = torch.device(device)
         self.model_id = model_id
 
-        self.model, self.processor = self._setup()
+        self.feature_extractor, self.processor, self.model = self._setup()
 
         self.level_tweak = level_tweak
 
     def _setup(self):
         processor = WhisperProcessor.from_pretrained(self.model_id)
+        feature_extractor = AutoFeatureExtractor.from_pretrained(self.model_id)
         model = WhisperForConditionalGeneration.from_pretrained(self.model_id)
 
-        return processor, model
+        return processor, feature_extractor, model
