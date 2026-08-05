69e308795e9484936fcd4e1853280f50a427ca19
look-luc <denardiluc@gmail.com>
2026-08-05 16:16:05 -0600

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 from graph_metric.graph import metrics_graph
@@ -10,4 +11,19 @@ hf_token = os.getenv("HF_TOKEN")
 def run_model(what_model:str):
     french_speech_transcription = French_Speech_text()
     if what_model == "train":
-        french_sppech_transcription.train()
+        try:
+            french_speech_transcription.train()
+            save_status = f"Successfully trained model setup: {french_speech_transcription.model_id}"
+        except Exception as e:
+            save_status = f"ERROR with model: {str(e)}"
+
+        output_dir = Path("./model/french_speech_transcription_output")
+        output_dir.mkdir(parents=True, exist_ok=True)
+        with open(output_dir / "model_out.txt", "w", encoding="utf-8") as file:
+            file.write(save_status)
+    elif what_model == "transcribe":
+        pass
+    elif what_model == "graph":
+        metrics_graph()
+    else:
+        raise ValueError(f"{what_model.capitalize()} is not one of the options. Only pick one of these: train, transcribe, or graph.")
