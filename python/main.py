db5c89146b30359639979d88186eb1626737cf3b
look-luc <denardiluc@gmail.com>
2026-08-10 09:15:14 -0600

    adding way to see error

--- python/main.py
+++ python/main.py
@@ -20,6 +20,8 @@ def run_model(what_model:str):
             save_status = f"ERROR with model: {str(e)}"
         output_dir = Path("./model/french_speech_transcription_base_output")
         output_dir.mkdir(parents=True, exist_ok=True)
+        with open(output_dir / "model_out.txt", "w", encoding="utf-8") as file:
+            file.write(save_status)
     elif what_model == "train":
         french_speech_transcription = French_Speech_text()
         try:
