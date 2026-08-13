1e3fe1785b67375d9f0fcbc5b5a064848ea8dc87
look-luc <denardiluc@gmail.com>
2026-08-13 10:39:25 -0600

    trying to get consent down plus some testing of mic

--- experiment/frontend/index.js
+++ experiment/frontend/index.js
@@ -1,18 +1,70 @@
-var jsPsych = initJsPsych({
-  on_finish: function () {
-    jsPsych.data.displayData();
-  },
-});
+const consentGranted = sessionStorage.getItem("consent_granted");
+if (consentGranted !== "true") {
+  window.location.href = "consent.html";
+} else {
+  var jsPsych = initJsPsych({
+    on_finish: function () {
+      sessionStorage.removeItem("consent_granted");
+      jsPsych.data.displayData();
+    },
+  });
 
-/* create timeline */
-var timeline = [];
+  jsPsych.data.addProperties({
+    consent_given: true,
+    consent_timestamp: sessionStorage.getItem("consent_timestamp"),
+  });
+  var timeline = [];
 
-/* define welcome message trial */
-var welcome = {
-  type: jsPsychHtmlKeyboardResponse,
-  stimulus: "Welcome to the experiment. Press any key to begin.",
-};
-timeline.push(welcome);
+  var init_mic = {
+    type: jsPsychInitializeMicrophone,
+    device_select_message: "Please select your microphone:",
+    button_label: "Use this microphone",
+  };
+  timeline.push(init_mic);
 
-/* start the experiment */
-jsPsych.run(timeline);
+  var test_mic = {
+    type: jsPsychHtmlAudioResponse,
+    stimulus: `
+        <div class="recording-container">
+          <span class="rec-dot"></span>
+          <span class="rec-text">Recording <p id="record"></p></span>
+        </div>
+        <p>Please say <strong>"test"</strong> to check your microphone.</p>
+      `,
+    recording_duration: 5000,
+    allow_playback: true,
+  };
+
+  var countDownDate = new Date();
+  countDownDate.setSeconds(currentTime.getSeconds() + 5);
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("record").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("record").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
+  timeline.push(test_mic);
+
+  var welcome = {
+    type: jsPsychHtmlKeyboardResponse,
+    stimulus: "Welcome to the experiment. Press any key to begin.",
+  };
+  timeline.push(welcome);
+
+  jsPsych.run(timeline);
+}
