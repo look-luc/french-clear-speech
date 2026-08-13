6013207035274939a70cacc1e2cf4da8a5586127
look-luc <denardiluc@gmail.com>
2026-08-13 15:59:27 -0600

    added french text

--- experiment/frontend/index.js
+++ experiment/frontend/index.js
@@ -17,8 +17,9 @@ if (consentGranted !== "true") {
 
   var init_mic = {
     type: jsPsychInitializeMicrophone,
-    device_select_message: "Please select your microphone:",
-    button_label: "Use this microphone",
+    device_select_message:
+      "Please select your microphone/S'il vous plaît choisir votre microphone:",
+    button_label: "Use this microphone/Utilisez cette microphone",
   };
   timeline.push(init_mic);
 
@@ -33,6 +34,7 @@ if (consentGranted !== "true") {
           <span id="timer-display" class="timer">5 seconds</span>
         </div>
         <p>Please say <strong>"test"</strong> to check your microphone.</p>
+        <p>S'il vous plaît dis <strong>"test"</strong> pour le vérification votre microphone
       `,
     recording_duration: 5000,
     allow_playback: true,
@@ -56,7 +58,14 @@ if (consentGranted !== "true") {
 
   var welcome = {
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: "Welcome to the experiment. Press any key to begin.",
+    stimulus: `<div class="welcome-eng">
+        <p><strong>Welcome to the experiment.</strong></p>
+        <p>You will be prompted to say a phrase as clear as possible given the situation that the phrase is in. You have only ONE (1) chance to record. Once you are done recording, a transcription will show with the percentage of what the "listener" heard what you said.</p>
+        <p>Press any key to begin.</p>
+        <p><strong>Bienvenue a l'expérience.</strong></p>
+        <p>Vous serez invité à dire une phrase aussi clair que possible pendant la situation que la phrase est dans. Vous avez juste UNE (1) chance pour enregistrer. Quand vous avez terminé, une transcription va montrer avec le pourcentage de quoi la "personne qui l'écoute" a écouté que vous avez dire.</p>
+        <p>Poussez n'importe quelle touche pour commencer.</p>
+      </div>`,
   };
   timeline.push(welcome);
 
