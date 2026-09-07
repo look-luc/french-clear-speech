9615bacd571ad0549af5f5aefee6853d6c6ba16c
look-luc <denardiluc@gmail.com>
2026-09-07 13:39:15 -0600

    added way to find out what the predictions were

--- model/transcription_model/base_model.py
+++ model/transcription_model/base_model.py
@@ -250,4 +250,16 @@ class French_Speech_text_base:
 
         cer_score = {"tache 1": cer_score_tache1, "tache 2": cer_score_tache2}
         wer_score = {"tache 1": wer_score_tache1, "tache 2": wer_score_tache2}
-        return f"CER: {cer_score}\nWER: {wer_score}"
+        return f"CER: {
+            cer_score
+        }\nWER: {
+            wer_score
+        }\nlabel for tache 1: {
+            references_tache1
+        }\npredictions for tache 1: {
+            predictions_tache1
+        }\nlabel for tache 2: {
+            references_tache2
+        }\npredictions for tache 2: {
+            predictions_tache2
+        }"
