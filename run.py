88fd6a21f594d770117fa39f83d45bba999ef679
look-luc <denardiluc@gmail.com>
2026-09-11 11:25:14 -0600

    fixing some issues with argurments

--- run.py
+++ run.py
@@ -5,6 +5,18 @@ import torch
 
 from model.main import run_model
 
+
+def parse_multiple_types(value):
+    try:
+        return int(value)
+    except ValueError:
+        pass
+    try:
+        return float(value)
+    except ValueError:
+        pass
+    return value
+
 if __name__ == "__main__":
     gc.collect()
     torch.cuda.empty_cache()
@@ -12,8 +24,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run transcription model pipeline")
     parser.add_argument("--model_type", type=str, default="train")
     parser.add_argument("--noise_type", type=str, default="studio")
-    parser.add_argument("--cutoff_freq", type=int, default=None)
-    parser.add_argument("--snr_db", type=int, default=None)
+    parser.add_argument("--cutoff_freq", type=parse_multiple_types, default=None)
+    parser.add_argument("--snr_db", type=parse_multiple_types, default=None)
     args = parser.parse_args()
 
     print(f"running {args.model_type}, with {args.noise_type}, {args.cutoff_freq}, and {args.snr_db}\n\n")
