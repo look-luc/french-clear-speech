5a4cb3a2fc99c237a6294f9725c7f1c38a7b9de9
look-luc <denardiluc@gmail.com>
2026-09-14 20:35:52 -0600

    adding a couple more background noices

--- model/transcription_model/model_experiment.py
+++ model/transcription_model/model_experiment.py
@@ -74,22 +74,35 @@ class French_Clear_Speech_Model:
         degraded_audio = audio_array.detach().cpu().numpy()
 
         def _simulate_noise_env(noise_type: str):
-            snr_db, cutoff_freq = 0, 0
-            if noise_type.lower() == "studio":
-                snr_db = 30
-                cutoff_freq = None
-            elif noise_type.lower() == "mild_office":
-                snr_db = 15
-                cutoff_freq = 3400
-            elif noise_type.lower() == "moderate_cafe":
-                snr_db = 10
-                cutoff_freq = 1500
-            elif noise_type.lower() == "severe_street":
-                snr_db = 0
-                cutoff_freq = 800
-            elif noise_type.lower() == "extreme_cocktail":
-                snr_db = -5
-                cutoff_freq = 500
+            snr_db, cutoff_freq = None, None
+            match noise_type.lower():
+                case "broadcast":
+                    snr_db = 45
+                    cutoff_freq = None
+                case "high_end_studio":
+                    snr_db = 35
+                    cutoff_freq = 7000
+                case "studio":
+                    snr_db = 30
+                    cutoff_freq = None
+                case "quiet_home":
+                    snr_db = 25
+                    cutoff_freq = 5500
+                case "library":
+                    snr_db = 20
+                    cutoff_freq = 4500
+                case "mild_office":
+                    snr_db = 15
+                    cutoff_freq = 3400
+                case "moderate_cafe":
+                    snr_db = 10
+                    cutoff_freq = 1500
+                case "severe_street":
+                    snr_db = 0
+                    cutoff_freq = 800
+                case "extreme_cocktail":
+                    snr_db = -5
+                    cutoff_freq = 500
             return snr_db, cutoff_freq
 
         if noise_type is not None:
