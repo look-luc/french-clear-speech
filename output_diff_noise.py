5a1fb3229cfc80c0c88288da7d938838eecd2481
look-luc <denardiluc@gmail.com>
2026-09-14 20:36:04 -0600

    added results from original simulations

---
+++ output_diff_noise.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+output = {
+    'studio': {
+        'transcription':['générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président'],
+        'avg confidence': np.float64(0.5197942945692274)
+    },
+    'mild_office': {
+        'transcription':['générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président'],
+        'avg confidence': np.float64(0.5197996788554722)
+    },
+    'moderate_cafe': {
+        'transcription':['générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président'],
+        'avg confidence': np.float64(0.5197364449501037)
+    },
+        'severe_street': {
+            'transcription': ['générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président'],
+            'avg confidence': np.float64(0.5172011329068078)
+        },
+        'extreme_cocktail': {
+            'transcription': ['générique', 'monsieur le président', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'monsieur le président', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'générique', 'monsieur le président', 'générique', 'générique'],
+            'avg confidence': np.float64(0.5128406292862362)
+        }
+}
+
+valid_out = {
+    outer_key: {k: v for k, v in inner_dict.items() if k != "transcription"}
+    for outer_key, inner_dict in output.items()
+}
+
+for noise_type, values in valid_out.items():
+    valid_out[noise_type]['avg confidence'] = valid_out[noise_type]['avg confidence'].item()
+
+print(valid_out)
+
+"""
+{
+    'studio': {
+        'avg confidence': 51.97942945692274
+    },
+    'mild_office': {
+        'avg confidence': 51.97996788554722
+    },
+    'moderate_cafe': {
+        'avg confidence': 51.97364449501037
+    },
+    'severe_street': {
+        'avg confidence': 51.72011329068078
+    },
+    'extreme_cocktail': {
+        'avg confidence': 51.28406292862362
+    }
+}
+"""
